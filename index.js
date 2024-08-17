022537c96dd20466b92cda62e809dccb8d3a5ed7
1010 <134185805+jedenzero@users.noreply.github.com>
2024-08-17 23:08:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var codes = [];
 
 function setCode(){
   let raw_code = document.querySelector('#code-textarea').value;
-
+  codes=[];
+  
   raw_code.split('\n').forEach(row=>{
     codes.push([row.split(': ')[0], row.split(': ')[1]]);
   });
@@ -14,22 +15,44 @@ function setCode(){
 
 function drawLetters(){
   let input = document.querySelector('#input-textarea').value;
+  x = 0;
+  y = font_size;
   
   while(input!=''){
     for(const code of codes){
-      const codeReg=new RegExp(`^${code[0]}`);
+      const codeReg = new RegExp(`^${code[0]}`);
       if(codeReg.test(input)){
-        input=input.replace(codeReg,'');
+        input = input.replace(codeReg,'');
         drawLetter(code[1]);
         break;
       }
       if(code==codes[codes.length-1]){
-        input=input.slice(1);
+        input = input.slice(1);
       }
     }
   }
 }
 
 function drawLetter(code){
-  
+  const ctx = document.querySelector('#output-canvas').getContext('2d');
+  ctx.lineWidth = font_weight*font_size/10;
+  code.split(', ').forEach(stroke=>{
+    ctx.beginPath();
+    ctx.moveTo(movePoint(stroke.split(' ')[0]));
+    stroke.split(' ').slice(1).forEach(command=>{
+      if(command.startsWith('L')){
+        ctx.lineTo(movePoint(command.slice(1)));
+      }
+      if(command.startsWith('C')){
+        //arc
+      }
+    });
+  });
+  x += kerning;
+}
+
+function movePoint(point_before){
+  point_before = point_before.slice(1, -1).split(',').map(Number);
+  const point_after = [point_before[0]*font_size/8+x, point_before[1]*font_size/8+y]
+  return point_after;
 }
